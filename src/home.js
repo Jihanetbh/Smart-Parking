63c613074321617ccf7f5ba8cc714ea01d934af7
Jihane Ammaoui <jihaneammaoui659@gmail.com>
2023-04-09 16:32:34 +0000

    Home

--- src/home.js
+++ src/home.js
@@ -7,26 +7,26 @@ function Home() {
     <div className="home">
       <div className="top-bar">
         <div className="logo">
-          <img src={spLogo} alt="Smart Parking Logo" style={{ height: "150px" }} />
+          <img src={spLogo} alt="Smart Parking Logo" style={{ height: "50px" }} />
         </div>
         <div className="nav-links">
           <a href="/home">
-            <button className="top-bar-button" style={{ height: "150px" }}>Home</button>
+            <button className="top-bar-button" >Home</button>
           </a>
           <a href="/book">
-            <button className="top-bar-button" style={{ height: "150px" }}>Book</button>
+            <button className="top-bar-button" >Book</button>
           </a>
           <a href="/account">
-            <button className="top-bar-button" style={{ height: "150px" }}>Account</button>
+            <button className="top-bar-button" >Account</button>
           </a>
           <a href="/about">
-            <button className="top-bar-button" style={{ height: "150px" }}>About</button>
+            <button className="top-bar-button" >About</button>
           </a>
           <a href="/contact">
-            <button className="top-bar-button" style={{ height: "150px" }}>Contact</button>
+            <button className="top-bar-button" >Contact</button>
           </a>
           <a href="/signin">
-            <button className="top-bar-button" style={{ height: "150px" }}>Sign Out</button>
+            <button className="top-bar-button" >Sign Out</button>
           </a>
         </div>
       </div>
