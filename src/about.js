1b5f90e7cb7cc802b46d3e0bb1631362c86ac3ff
Jihane Ammaoui <jihaneammaoui659@gmail.com>
2023-04-11 22:54:07 +0000

    Base done

--- src/about.js
+++ src/about.js
@@ -1,3 +1,4 @@
+import SP from './SP.svg';
 import React from "react";
 import "./about.css";
 
@@ -5,12 +6,17 @@ function about() {
   return (
     <div className="about">
       <div className="top-bar">
+        <div className="logo">
+          <img src={SP} alt="Smart Parking Logo" style={{ height: "50px" }} />
+        </div>
+        <div className="nav-links">
         <a href="/home"><button className="top-bar-button">Home</button></a>
         <a href="/book"><button className="top-bar-button">Book</button></a>
         <a href="/account"><button className="top-bar-button">Account</button></a>
         <a href="/about"><button className="top-bar-button">About</button></a>
         <a href="/contact"><button className="top-bar-button">Contact</button></a>
         <a href="/signin"><button className="top-bar-button">Sign Out</button></a>
+      </div>
       </div>
     </div>
   );
