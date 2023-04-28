1f547f0a9c53ccd9be5147a9449dd126784cf873
Lalaalive <saraboukili183@gmail.com>
2023-04-28 04:16:28 +0100

    test

--- src/book.js
+++ src/book.js
@@ -2,24 +2,93 @@ import SP from './SP.svg';
 import React from "react";
 import "./book.css";
 
-function book() {
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
   return (
-    <div className="book">
+    <Router>
+      <div className="book">
+        <BookTopBar />      
+        <Switch>
+          <Route path="/account">
+            <RegistrationForm />
+          </Route>
+          <Route path="/">
+            <BookPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function BookTopBar(){
+  return(
     <div className="top-bar">
-        <div className="logo">
-          <img src={SP} alt="Smart Parking Logo" style={{ height: "50px" }} />
-        </div>
-        <div className="nav-links">
-        <a href="/home"><button className="top-bar-button">Home</button></a>
-        <a href="/book"><button className="top-bar-button">Book</button></a>
-        <a href="/account"><button className="top-bar-button">Account</button></a>
-        <a href="/about"><button className="top-bar-button">About</button></a>
-        <a href="/contact"><button className="top-bar-button">Contact</button></a>
-        <a href="/signin"><button className="top-bar-button">Sign Out</button></a>
-      </div>
-      </div>
+    <div className="logo">
+      <img src={SP} alt="Smart Parking Logo" style={{ height: "50px" }} />
+    </div>
+    <div className="nav-links">
+    <a href="/home"><button className="top-bar-button">Home</button></a>
+    <a href="/book"><button className="top-bar-button">Book</button></a>
+    <a href="/account"><button className="top-bar-button">Account</button></a>
+    <a href="/about"><button className="top-bar-button">About</button></a>
+    <a href="/contact"><button className="top-bar-button">Contact</button></a>
+    <a href="/signin"><button className="top-bar-button">Sign Out</button></a>
+  </div>
+  </div>
+  )
+}
+function BookPage() {
+  return (
+    <div>
+      <ClickableImage
+        src="./carrefourparkimg.jpg"
+        alt="Image 1"
+        text="Carrefour Parking"
+        link="/account"
+      />
+      <ClickableImage
+        src="./marjaneparkimg.jpg"
+        alt="Image 2"
+        text="Marjane Parking"
+        link="/account"
+      />
     </div>
   );
 }
 
-export default book;
+function ClickableImage(props) {
+  return (
+    <Link to={props.link}>
+      <img src={props.src} alt={props.alt} />
+      <p>{props.text}</p>
+    </Link>
+  );
+}
+
+function RegistrationForm() {
+  return (
+    <div>
+      <h1>Registration Form</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
